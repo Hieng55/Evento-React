1d147e15c7906f0fd73063b2e3db57974655b5e4
Hieng55 <bosshieng55@gmail.com>
2024-01-20 22:52:44 +0700

    update funtion button

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -2,8 +2,10 @@ import { useRef, useState } from "react";
 
 export const Counter = () => {
   const [count, setCount] = useState<number>(4);
+  const [isHidden, setIsHidden] = useState<boolean>(false);
   const intervalId = useRef<number | undefined>(undefined);
   function play() {
+    setIsHidden(true);
     intervalId.current = setInterval(() => {
       setCount((prevCount) => {
         if (prevCount === 0) {
@@ -15,22 +17,30 @@ export const Counter = () => {
     }, 1000);
   }
   function pause() {
+    console.log("pause");
+
+    setIsHidden(false);
     clearInterval(intervalId.current);
   }
   function reset() {
+    setIsHidden(false);
     clearInterval(intervalId.current);
     setCount(4);
   }
+
   return (
     <div>
       <h3 className="text-center text-2xl mb-2">{count}</h3>
       <div className="flex gap-1">
-        <button onClick={play} className="text-white bg-orange-500 p-2">
-          Play
-        </button>
-        <button onClick={pause} className="text-white bg-orange-500 p-2">
-          Pause
-        </button>
+        {!isHidden ? (
+          <button onClick={play} className="text-white bg-orange-500 p-2">
+            Play
+          </button>
+        ) : (
+          <button onClick={pause} className="text-white bg-orange-500 p-2">
+            Pause
+          </button>
+        )}
         <button onClick={reset} className="text-white bg-orange-500 p-2">
           Reset
         </button>
